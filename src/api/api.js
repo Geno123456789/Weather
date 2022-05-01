d070a0e81787a4a6a0bc3eb3a327b91f92eb66ea
Geno <pisareviczena@gmail.com>
2022-05-01 18:42:03 +0300

    made a request for a forecast

--- src/api/api.js
+++ src/api/api.js
@@ -7,11 +7,13 @@ const instance = axios.create({
 export const weatherApi = {
     getCityName(town = 'Minsk') {
         return instance.get(`weather?q=${town}&units=metric&appid=f570f866ddbd1afb342a8cb2799dcf38`)
+    },
+    forWeek(city){
+        return instance.get(`forecast?q=${city}&lang=ru&units=metric&appid=471fb871540b1cabfd3ed0bce031cb0d`)
+        .then( res => res.data.list ) 
     }
 }
 
 // "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f570f866ddbd1afb342a8cb2799dcf38"
-
-
 
 
